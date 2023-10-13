d2b8ba7f26a89a1ffc814e4b0a0e5860d2341f7a
tan350 <55528524+tan350@users.noreply.github.com>
2023-10-13 15:19:14 +0530

    Add files via upload

---
+++ Assignment0 Out Keyword/Assignment0 Out Keyword/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Assignment0_Out_Keyword
+{
+    internal class Program
+    {
+        public static void Sum(out int num1)
+        {
+            num1 = 80;
+            num1 = num1 + 1;
+        }
+        static void Main(string[] args)
+        {
+            int num1;
+            Sum(out num1);
+
+            Console.WriteLine("The Number is: " + num1);
+        }
+
+    }
+}
