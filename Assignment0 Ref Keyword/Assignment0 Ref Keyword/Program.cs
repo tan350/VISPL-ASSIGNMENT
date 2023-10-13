de45197a08354363a07c850d2d5abf1a8f0f9b8e
tan350 <55528524+tan350@users.noreply.github.com>
2023-10-13 15:22:40 +0530

    Add files via upload

---
+++ Assignment0 Ref Keyword/Assignment0 Ref Keyword/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Assignment0_Ref_Keyword
+{
+    internal class Program
+    {
+        static void SetValue(ref string str1)
+        {
+            if (str1 == "Ram")
+            {
+                Console.WriteLine("Strig is Ram");
+            }
+
+            str1 = "Sita Ram";
+        }
+        static void Main(string[] args)
+        {
+            string str = "Ram";
+
+            SetValue(ref str);
+            Console.WriteLine(str);
+        }
+
+    }
+}
